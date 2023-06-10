4ed84ff43ba7a663492b18977d64fb48d6e2abc4
MartaKoss27 <27691497m@gmail.com>
2023-06-10 17:10:10 +0200

    update 01-gallery-js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<li class="list-item"><img src= ${item.preview} alt= ${item.description} width = 400px ></img></li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+console.log(gallery);
